9224247d22266a2383a8dcc49ef54d74b1539ecc
Zshoham <ShohamZarfti@gmail.com>
2018-01-16 16:32:41 +0200

    added product functions.

--- Product.java
+++ Product.java
@@ -1,12 +1,12 @@
 public abstract class Product {
 
-	private String name;
-	private int serialNum;
-	private double price;
+	private String name; //the name of the product.
+	private int serialNum; // the serial number of the product.
+	private double price; //the price of the product.
 	
 	
 	/**
-	 * constructor
+	 * default constructor
 	 * @param name
 	 * @param serialNum
 	 * @param price
@@ -18,23 +18,61 @@ public abstract class Product {
 	}
 
 	/**
-	 * constructor
+	 * copy constructor
 	 * @param product
 	 */
 	public Product(Product product) {
 	    this(product.name, product.serialNum, product.price);
     }
-	
+
+    /**
+     * @return the name of the product
+     */
+    public String getProductName() {
+	    return this.name;
+    }
+
+    /**
+     * @return the serial number of the product.
+     */
+    public int getProductSerialNumber() {
+	    return this.serialNum;
+    }
+
 	/**
-	 * @return
+	 * @return returns the price of the product (without shipping price).
 	 */
 	public double getProductPrice() {
 		return this.price;
 	}
 
 	/**
-	 * @return 
+	 * @return the final price of the product (includes shipping price).
 	 */
 	public abstract double[] computeFinalPrice();
+
+    /**
+     * @return a string describing the Product.
+     */
+	@Override
+    public String toString() {
+	    return "Product: " + this.name + ", " + this.serialNum + ", " + this.price;
+    }
+
+    /**
+     * checks if the product other is equal to this product, if other is a Product.
+     * @param other the product being compared with.
+     * @return true if the products are the same and false otherwise.
+     */
+    @Override
+    public boolean equals(Object other) {
+	    if(other instanceof Product) {
+            Product otherProduct = (Product)other;
+            if(this.name.equals(otherProduct.name) &&
+                    this.serialNum == otherProduct.serialNum &&
+                    this.price == otherProduct.price) return true;
+        }
+        return false;
+    }
 	
 }
