84e0534d47c375b1106cbb5e570acd85a372470d
hodTwito <35403303+hodTwito@users.noreply.github.com>
2018-01-16 16:14:28 +0200

    commit

--- Product.java
+++ Product.java
@@ -1,30 +1,40 @@
 public abstract class Product {
 
-	// Fields
 	private String name;
 	private int serialNum;
 	private double price;
 	
 	
-	// Constructors
+	/**
+	 * constructor
+	 * @param name
+	 * @param serialNum
+	 * @param price
+	 */
 	public Product(String name, int serialNum, double price){
 		this.name = name;
 		this.serialNum = serialNum;
 		this.price = price;
 	}
 
+	/**
+	 * constructor
+	 * @param product
+	 */
 	public Product(Product product) {
 	    this(product.name, product.serialNum, product.price);
     }
 	
+	/**
+	 * @return
+	 */
 	public double getProductPrice() {
 		return this.price;
 	}
 
-	// Methods
+	/**
+	 * @return 
+	 */
 	public abstract double[] computeFinalPrice();
 	
-	/*
-	* ADD YOUR METHODS BELOW
-	*/
 }
